6acb1c842cdb128457e0ac926e8f6f75ab9d9a62
Xida Ren <cedar.ren@gmail.com>
2018-03-22 14:39:16 -0400

    changed dependency to network-online.target for correct network-waiting behavior

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 USER_SYSTEMD_DIR=$HOME/.config/systemd/user/
 mkdir -p $USER_SYSTEMD_DIR # create systemd user dir
 cp ./labtunnel@.service $USER_SYSTEMD_DIR
-echo "Successfully installed systemd template labtunnel@.service in ~/.config/systemd/user"
+echo "Successfully installed systemd template labtunnel@.service in ~/.config/systemd/user."
+echo "Questions? Raise an issue at github.com/renxida/labtunnel"
