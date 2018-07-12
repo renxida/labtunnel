d26eb2478ce23b94dd2fab68381d8c9d3b114dd0
Petr Vasiliev <petr-wasilew@yandex.ru>
2018-07-12 12:00:47 +0300

    Add ssh-agent config example Improve install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 USER_SYSTEMD_DIR=$HOME/.config/systemd/user/
 mkdir -p $USER_SYSTEMD_DIR # create systemd user dir
 cp ./labtunnel@.service $USER_SYSTEMD_DIR
+systemctl --user daemon-reload
 echo "Successfully installed systemd template labtunnel@.service in ~/.config/systemd/user."
 echo "Questions? Raise an issue at github.com/renxida/labtunnel"
